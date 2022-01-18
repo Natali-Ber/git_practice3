90d14b75b4f435d088617af60347351d6f8a4508
Natalia <berluchy@gmail.com>
2022-01-18 16:21:11 -0600

    test 1

--- src/day3/TimeMachine.java
+++ src/day3/TimeMachine.java
@@ -9,6 +9,7 @@ public class TimeMachine {
     // work 1
     // work 2
     // work 3
+    // work 4
 
 
 
--- src/day3/apple.java
+++ src/day3/apple.java
@@ -1,5 +1,8 @@
 package day3;
 
 public class apple {
+    public static void main(String[] args) {
+ //apple 1
+    }
 
 }
